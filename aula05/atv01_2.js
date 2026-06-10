c34a44f98b979f1170cfc4612a1ff49ee11868e3
danrley0595 <danrleycramos@gmail.com>
2026-06-09 22:01:19 -0300

    Add files via upload

---
+++ aula05/atv01_2.js
@@ -0,0 +1,20 @@
+let tarefa = document.getElementById("inputTarefa");
+let listaTarefa = document.getElementById("lista-tarefa");
+
+function adicionarItem(){
+    let itemTarefa = document.createElement("li");
+    itemTarefa.textContent = tarefa.value;
+    listaTarefa.appendChild = (itemTarefa);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
